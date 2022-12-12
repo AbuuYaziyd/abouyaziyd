8577d0f7221ee4ea72529210db5dd9e4552b5d3b
Abou Yaziyd <gakibrush@gmail.com>
2022-12-13 00:55:28 +0300

    prog

--- abouyaziyd.rf.gd/htdocs/app/Models/Admin.php
+++ abouyaziyd.rf.gd/htdocs/app/Models/Admin.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class User extends Model
+class Admin extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'users';
+    protected $table            = 'admins';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
