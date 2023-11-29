a9bd3d79deaad3fd989bb15b9c33ffe8308ff822
Abou Yaziyd <gakibrush@gmail.com>
2023-11-30 01:41:06 +0300

    pages

---
+++ app/Language/en/app.php
@@ -0,0 +1,22 @@
+<?php
+
+// override core en language system validation or define your own en language validation message
+return [
+    'webDev' => 'Web DEV',
+    'home' => 'Home',
+    'about' => 'About',
+    'services' => 'Services',
+    'service' => 'Service',
+    'skill' => 'Skill',
+    'skills' => 'Skills',
+    'education' => 'Education',
+    'educations' => 'Educations',
+    'blog' => 'Blog',
+    'blogs' => 'Blogs',
+    'work' => 'Work',
+    'works' => 'Works',
+    'experience' => 'Experience',
+    'experiences' => 'Experiences',
+    'contact' => 'Contact',
+    'contacts' => 'Contacts',
+];
---
+++ app/Views/layouts/sidebar.php
@@ -0,0 +1,15 @@
+<nav id="colorlib-main-menu" role="navigation" class="navbar">
+    <div id="navbar" class="collapse">
+        <ul>
+            <li class="active"><a href="#" data-nav-section="<?= lang('app.home') ?>"><?= lang('app.home') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.about') ?>"><?= lang('app.about') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.services') ?>"><?= lang('app.services') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.skills') ?>"><?= lang('app.skills') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.education') ?>"><?= lang('app.education') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.experience') ?>"><?= lang('app.experience') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.work') ?>"><?= lang('app.work') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.blog') ?>"><?= lang('app.blog') ?></a></li>
+            <li><a href="#" data-nav-section="<?= lang('app.contact') ?>"><?= lang('app.contact') ?></a></li>
+        </ul>
+    </div>
+</nav>
