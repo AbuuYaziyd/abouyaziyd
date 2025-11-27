8154682bcee926d063c0217050b5cfdd740642f7
Abou Yaziyd <gakibrush@gmail.com>
2025-11-27 07:07:21 +0300

    darsa table

--- app/Database/Migrations/2025-11-27-040504_DarsaTable.php
+++ app/Database/Migrations/2025-11-27-040504_DarsaTable.php
@@ -8,11 +8,32 @@ class DarsaTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'category_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'unique' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+            'created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('darsa');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('darsa');
     }
 }
